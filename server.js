f71f66d4a8f2c7eb5a9195503a57ccf33fbac4a3
r2182 <30991532+r2182@users.noreply.github.com>
2017-08-20 17:20:46 +0930

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,13 +107,13 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name', function(req,res) {
+app.get('/submit-name/:name', function(req,res) {
     //Get the name from the request
-    var name; 
+    var name = req.params.name;
     
     names.push(name);
-    
-    res.send(names);
+    //JSON: Javascript Object Notation
+    res.send(JSON.stringify(names));
 }
 
 
