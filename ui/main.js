ea80e8740e18e99700fbcdfc5cc80b44b6988da1
r2182 <30991532+r2182@users.noreply.github.com>
2017-08-20 17:45:13 +0930

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -65,7 +65,7 @@ submit.onclick = function () {
                 names = JSON.parse(names);
                 var list = '';
                 for (var i=0; i<names.length; i++) {
-                    list += '<li>' + names[i];
+                    list += '<li>' + names[i] + '<li>';
                 }
                 var ul = document.getElementById('namelist');
                 ul.innerHTML = list;
